381f8b36523af611f9aa19bb2e9a8df719dda670
in-vishnu-prasad <vishnu.p@inscholaris.com>
2022-03-14 10:42:04 +0530

    Added Endpoints for Register and Login

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express"),
+app = express();
+
+require("dotenv").config();
+
+mongoose = require("mongoose");
+userRoutes = require("./routes/user");
+
+try {
+    mongoose.connect("mongodb://localhost:27017/MoviesDB?readPreference=primary&appname=MongoDB%20Compass&ssl=false", {
+      useUnifiedTopology: true,
+      useNewUrlParser: true
+    });
+    console.log("Connected to DB");}
+catch (error) {
+    handleError(error);
+    }
+    process.on('unhandledRejection', error => {
+        console.log('unhandledRejection', error.message);
+    });
+  
+
+app.use(express.json());
+  
+app.use(express.urlencoded({
+extended: true
+}));
+
+app.use(userRoutes);
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is live on port ${process.env.PORT}`);
+})
